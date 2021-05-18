3c851e285d3d4a9e2ef0abcd3999a1f732b8a695
Bruno Goncalves <P2510079@my365.dmu.ac.uk>
2021-05-18 01:33:22 +0100

    Fixed the home page, and added an option to download my cv. Created an alternative header with by adding a function and fixing the header, and then creating an alternative style for that new fixed header on the css page

--- Cw 2/script.js
+++ Cw 2/script.js
@@ -57,3 +57,16 @@ galleryItem.forEach(function(element){
 })
 
 //header fixed
+
+window.onscroll=function(){
+  const docScrollTop=document.documentElement.scrollTop;
+
+  if(window.innerWidth>991){
+    if(docScrollTop>100){
+      document.querySelector("header").classList.add("fixed")
+    }
+    else{
+      document.querySelector("header").classList.remove("fixed")
+    }
+  }
+}
