4dce9c8a45541aa5502b33775a2fadf8c0d67ec5
Bruno Goncalves <P2510079@my365.dmu.ac.uk>
2021-05-18 01:14:05 +0100

    Used Java script (const) and queryselector to tell the browser what photos to select and where to get them. After that eventlisteners to tell the browser on what action he should take action (click). Added the CSS code for the lightbox and fixed some bits of the html code in order for all of this to work together

--- Cw 2/script.js
+++ Cw 2/script.js
@@ -25,3 +25,35 @@ const target=this.getAttribute("data-target")
 
  })
 }
+
+//lightbox
+
+const closeLightbox=document.querySelector(".close-lightbox");
+const lightbox=document.querySelector(".lightbox");
+const lightboxImage=lightbox.querySelector("img")
+
+lightbox.addEventListener("click",function(){
+  if(event.target!=lightboxImage){
+    lightbox.classList.remove("show");
+    lightbox.classList.add("hide");
+}
+
+})
+
+  closeLightbox.addEventListener("click",function(){
+    lightbox.classList.remove("show");
+    lightbox.classList.add("hide");
+})
+
+const gallery=document.querySelector(".portfolio-gallery");
+const galleryItem=gallery.querySelectorAll(".item");
+
+galleryItem.forEach(function(element){
+   element.querySelector(".fa-plus").addEventListener("click",function(){
+    lightbox.classList.remove("hide");
+    lightbox.classList.add("show");
+    lightboxImage.src=element.querySelector("img").getAttribute("src")
+  })
+})
+
+//header fixed
